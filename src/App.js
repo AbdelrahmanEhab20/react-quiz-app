8940527bafbb484a4245c945b010aad8dd53e138
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-08-07 00:24:09 +0300

    setup our fake api and stare use Reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import DateCounter from "./Components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 
+const initialState = {
+  questions: [],
+  // # "loading" , "error" , "ready" , "active" , "finished"  ---- Status of the page of quiz
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "failed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("ERROR : ACTION NOT FOUND");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(initialState, reducer);
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "failed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/5</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
